3da06296abdbe762fed862ff31712bea13ae7091
Usuario <dscbvdfv@sdg.es>
2019-10-30 09:03:27 +0000

    Kata3 for base

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata4;
 
-/**
- *
- * @author Usuario
- */
 public class Kata4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Histogram<String> histogram = new Histogram<>();
+        
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        
+        HistogramDisplay display = new HistogramDisplay(histogram);
+        display.execute();
     }
     
 }
